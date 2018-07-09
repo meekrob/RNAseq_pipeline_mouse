35d989bd02376c5e724296a1e33a9d8491c2594f
Erin Nishimura <erinosb@gmail.com>
2018-07-09 12:31:53 -0600

    updated execution script

--- 02_scripts/execute_RNAseq_pipeline.sh
+++ 02_scripts/execute_RNAseq_pipeline.sh
@@ -12,11 +12,11 @@
 source /projects/dcking@colostate.edu/paths.bashrc
 
 ## execute the RNA-seq_pipeline
-#bash RNAseq_analyzer_mouse_180706.sh ../04_testing/metadata_mouse.txt 12
+bash RNAseq_analyzer_mouse_180706.sh ../04_testing/metadata_mouse.txt 12
      # modify the SECOND argument to point to YOUR metadata.file
      # modify the THIRD argument to indicate the number of THREADS you 
      # want to use. This number must match the number in #SBATCH --ntasks=#
 
 ## clean up by zipping .fastq files and deleting extra files
-bash RNAseq_cleanup_mouse_180706.sh ../04_testing/metadata_mouse.txt
+#bash RNAseq_cleanup_mouse_180706.sh ../04_testing/metadata_mouse.txt
      # modify the SECOND argument to point to YOUR metadata.file
